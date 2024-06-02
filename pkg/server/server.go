585adc01dae245a732138b031ccee9fe4ee15e8d
Naman Balaji <namanb487@gmail.com>
2024-06-02 17:19:03 +0200

    added server

---
+++ pkg/server/server.go
@@ -0,0 +1,50 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Trigger struct{}
+
+func RunServer(triggerChan chan Trigger, done chan struct{}) error {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/trigger", func(w http.ResponseWriter, r *http.Request) {
+		triggerChan <- Trigger{}
+		fmt.Println("Sent trigger")
+		w.WriteHeader(http.StatusOK)
+
+		return
+	})
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	// Run the server in a separate goroutine
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("ListenAndServe(): %v", err)
+		}
+	}()
+
+	fmt.Println("Server started on :8080")
+
+	<-done // Wait for the done signal
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		return fmt.Errorf("server shutdown failed: %w", err)
+	}
+
+	fmt.Println("Server stopped")
+
+	return nil
+}
--- src/main.go
+++ src/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
 	"github.com/go-rod/rod/lib/launcher"
 
@@ -11,6 +17,7 @@ import (
 	"github.com/NamanBalaji/drug-test-notifier/pkg/config"
 	"github.com/NamanBalaji/drug-test-notifier/pkg/data"
 	"github.com/NamanBalaji/drug-test-notifier/pkg/mail"
+	"github.com/NamanBalaji/drug-test-notifier/pkg/server"
 )
 
 func main() {
@@ -18,10 +25,42 @@ func main() {
 	debug := flag.Bool("debug", false, "Enable debug mode")
 	flag.Parse()
 	headless := false
-	if *debug {
+	if !*debug {
 		headless = true
 	}
+	triggerChan := make(chan server.Trigger)
+	done := make(chan struct{})
 
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		automate(cfg, triggerChan, done, headless)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := server.RunServer(triggerChan, done); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("Could not start server: %v\n", err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	<-sigChan
+	fmt.Println("Received shutdown signal")
+
+	// Signal the server and run function to stop
+	close(done)
+
+	// Wait for all goroutines to finish
+	wg.Wait()
+	fmt.Println("Server and processing gracefully stopped")
+}
+
+func automate(cfg config.Config, triggerChan chan server.Trigger, done chan struct{}, headless bool) {
 	browser, l := browser_automation.GetBrowser(headless)
 	if l != nil {
 		defer l.Cleanup()
@@ -34,22 +73,34 @@ func main() {
 	defer browser.MustClose()
 
 	d := data.NewData()
-	err := browser_automation.Run(cfg, browser, d)
-	if err != nil {
-		fmt.Println(err)
+
+	for {
+		select {
+		case <-triggerChan:
+			fmt.Println("Starting automation...")
+			err := browser_automation.Run(cfg, browser, d)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			mailClient := mail.NewMailClient(cfg.SenderEmail, cfg.AppPassword)
+
+			subject, body, err := d.GenerateEmail()
+			if err != nil {
+				log.Println("Error while browser automation: ", err)
+
+				continue
+			}
+
+			err = mailClient.SendMail(subject, body, cfg.Username)
+			if err != nil {
+				log.Println("Error while sending email: ", err)
+			}
+
+			fmt.Println("Email sent successfully!")
+		case <-done:
+			fmt.Println("Stopping processing")
+			return
+		}
 	}
-
-	mailClient := mail.NewMailClient(cfg.SenderEmail, cfg.AppPassword)
-
-	subject, body, err := d.GenerateEmail()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = mailClient.SendMail(subject, body, cfg.Username)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Email sent successfully!")
 }
